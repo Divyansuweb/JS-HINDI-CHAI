4504383b3ce856640c726aebbbccbcbc0e37729b
Divyansuweb <139138017+Divyansuweb@users.noreply.github.com>
2024-09-13 15:38:00 +0530

    Add files via upload

---
+++ 04_operations.js
@@ -0,0 +1,16 @@
+let value = 8;
+let nagValue = -value
+// console.log(nagValue);
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "Divyansu"
+let str2 = "Nidhi"
+let str3 = str1 + str2
+console.log(str3);
+
+console.log(+true); // 1
